074db1cd7d48a64ccea3099ae6ca186f51f7778b
Ho Viet Thien <thien.hoviet@gmail.com>
2018-06-21 00:15:02 +0700

    Add handle submit button

--- App.js
+++ App.js
@@ -3,12 +3,26 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
   state = {
-    placeName: ''
+    placeName: '',
+    places: []
   }
   placeNameChangeHandler = val => {
     this.setState({placeName: val});
   }
+  placeSubmitHandler = () => {
+    if(this.state.placeName.trim() === '') {
+      return;
+    }
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      }
+    });
+  }
   render() {
+    const placesOutput = this.state.places.map((place, i) => (
+      <Text key={i}>{place}</Text>
+    ));
     return (
       <View style={styles.container}>
         <View style={styles.inputContainer}>
@@ -17,7 +31,13 @@ export default class App extends React.Component {
             value={this.state.placeName}
             placeholder='Enter a text'
             onChangeText={this.placeNameChangeHandler}/>
-          <Button title='Add' style={styles.placeButton}/>
+          <Button
+            title='Add'
+            style={styles.placeButton}
+            onPress={this.placeSubmitHandler}/>
+        </View>
+        <View>
+          {placesOutput}
         </View>
       </View>
     );
