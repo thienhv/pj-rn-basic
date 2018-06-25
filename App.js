755fc1132665056a2de77ff51df27cb8a869d3fb
Ho Viet Thien <thien.hoviet@gmail.com>
2018-06-25 23:17:18 +0700

    Change place input

--- App.js
+++ App.js
@@ -1,73 +1,70 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import PlaceList from './src/components/PlaceList/PlaceList';
-import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
-import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-export default class App extends React.Component {
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
+
+export default class App extends Component {
   state = {
-    placeName: '',
     places: [],
     selectedPlace: null
-  }
-  placeNameChangeHandler = val => {
-    this.setState({placeName: val});
-  }
-  placeSubmitHandler = () => {
-    if(this.state.placeName.trim() === '') {
-      return;
-    }
+  };
+
+  placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
         places: prevState.places.concat({
           key: Math.random().toString(),
-          name: prevState.placeName,
+          name: placeName,
           image: {
             uri: 'http://www.watchoutladies.net/wp-content/uploads/2015/07/Beautiful-girl-makeup-for-blue-eyes.jpg'
           }
         })
-      }
+      };
     });
-  }
-  placeDeleteHandler = () => {
+  };
+
+  placeDeletedHandler = () => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place) => {
+        places: prevState.places.filter(place => {
           return place.key !== prevState.selectedPlace.key;
         }),
         selectedPlace: null
-      }
+      };
     });
-  }
-  onModalClosedHandler = () => {
+  };
+
+  modalClosedHandler = () => {
     this.setState({
       selectedPlace: null
     });
-  }
+  };
+
   placeSelectedHandler = key => {
     this.setState(prevState => {
       return {
-        selectedPlace: prevState.places.find((place) => {
-          return key === place.key;
+        selectedPlace: prevState.places.find(place => {
+          return place.key === key;
         })
-      }
+      };
     });
-  }
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <PlaceDetail
-          onItemDeleted={this.placeDeleteHandler}
-          onModalClosed={this.onModalClosedHandler}
-          selectedPlace={this.state.selectedPlace}/>
-        <PlaceInput
-          placeName={this.state.placeName}
-          onChangeText={this.placeNameChangeHandler}
-          onPress={this.placeSubmitHandler}
-          />
+          selectedPlace={this.state.selectedPlace}
+          onItemDeleted={this.placeDeletedHandler}
+          onModalClosed={this.modalClosedHandler}
+        />
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <PlaceList
+          places={this.state.places}
           onItemSelected={this.placeSelectedHandler}
-          places={this.state.places}/>
+        />
       </View>
     );
   }
@@ -76,9 +73,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    padding: 26,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
   }
 });
--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -1,35 +1,62 @@
-import React from 'react';
-import { StyleSheet, View, TextInput, Button } from 'react-native';
+import React, { Component } from "react";
+import { View, TextInput, Button, StyleSheet } from "react-native";
 
-const placeInput = props => {
-  return(
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.placeInput}
-        value={props.placeName}
-        placeholder='Enter a text'
-        onChangeText={props.onChangeText}/>
-      <Button
-        title='Add'
-        style={styles.placeButton}
-        onPress={props.onPress}/>
-    </View>
-  );
-};
+class PlaceInput extends Component {
+  state = {
+    placeName: ""
+  };
+
+  componentDidMount() {
+
+  }
+
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+
+    this.props.onPlaceAdded(this.state.placeName);
+  };
+
+  render() {
+    return (
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="An awesome place"
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+          style={styles.placeInput}
+        />
+        <Button
+          title="Add"
+          style={styles.placeButton}
+          onPress={this.placeSubmitHandler}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   inputContainer: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+    // flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
   },
   placeInput: {
-    width: '70%'
+    width: "70%"
   },
   placeButton: {
-    width: '30%'
+    width: "30%"
   }
 });
 
-export default placeInput;
+export default PlaceInput;
